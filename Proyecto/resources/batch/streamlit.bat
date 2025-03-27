3eea3779c974395bf58b98e270fb23857f013e64
fcastellanosp <111034731+fcastellanosp@users.noreply.github.com>
2025-03-27 07:18:33 -0500

    Update streamlit.bat

--- Proyecto/resources/batch/streamlit.bat
+++ Proyecto/resources/batch/streamlit.bat
@@ -3,9 +3,11 @@ cd..
 cd..
 setlocal
 set PROJECTPATH=%cd%
-set PYTHONPATH=%PYTHON310_HOME%;%cd%
-echo "Enviroment variable at '%PYTHONPATH%"
+set PYTHONDIR=%PYTHONPATH%
+echo "Python installed at: '%PYTHONPATH%'"
 set MAINPATH=%PROJECTPATH%\streamlit_app.py
-echo "Starting the app at '%MAINPATH%'"
-%PYTHON310_HOME%\python -m streamlit run "%MAINPATH%"
+set PATH=%PYTHONDIR%;%PATH%
+echo "My project path is: '%MAINPATH%'"
+python.exe -m streamlit run "%MAINPATH%"
 endlocal
+
